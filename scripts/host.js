27317f053ac871b04354ad389a10549c198ef750
Yash Chaudhari <yashchaudhari008@gmail.com>
2021-05-26 22:20:51 +0530

    Changed poolSize to 0.

--- scripts/host.js
+++ scripts/host.js
@@ -15,7 +15,7 @@ const servers = {
     iceServers: [{
         urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
     },],
-    iceCandidatePoolSize: 10,
+    iceCandidatePoolSize: 0,
 };
 
 let peerConnection = new RTCPeerConnection(servers);
